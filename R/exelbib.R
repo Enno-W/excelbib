4a3c2d85e5c635be3664db7e1bdb70c40fdc9506
Enno-W <ennomail95@yahoo.de>
2024-07-15 13:00:58 +0200

    In this commit, I corrected the dependandcies that are required. xlsx, openxlsx and utils are necessary, and I added them in the files "Description" and in the roxygen code, seen in "NAMESPACE"

--- R/exelbib.R
+++ R/exelbib.R
@@ -1,4 +1,4 @@
-#' Title
+#' Update your References from an Excel-file
 #'
 #' @param excel_file The name or directory of the Excel file you use for your References. In the file, the bibtex-entries should be arranged in a column, one entry per cell.
 #' @param bib_file The name of your .bib-file. Defaults to "bibliography.bib". The file will be added to your working directory.
@@ -8,21 +8,14 @@
 #'
 #' @return An error message if the file does not exist.
 #' @export
+#' @import xlsx openxlsx utils
 #'
 #' @examples
 #' download.file(url="https://raw.githubusercontent.com/Enno-W/excelbib/main/Excel_References_example.xlsx", destfile = "Excel_References_example.xlsx", mode ="wb")
 #' require("xlsx")
 #' xlsx_to_bib ("Excel_References_example.xlsx", bib_file="example_bibliography.bib", sheet = 2, column = 1, first_row = 2)
 xlsx_to_bib <- function(excel_file = "References.xlsx", bib_file = "bibliography.bib", sheet = 2, column = 1, first_row = 2) {
-  if (!requireNamespace("xlsx", quietly = TRUE)) {
-    stop(
-      "Package \"xlsx\" must be installed to use this function.",
-      call. = FALSE
-    )
-  }
-  if (!"xlsx" %in% loadedNamespaces()) {
-    library(xlsx)
-  }
+
   if (!file.exists(excel_file)) {
     stop("The specified Excel file does not exist.")
   }
@@ -35,14 +28,15 @@ xlsx_to_bib <- function(excel_file = "References.xlsx", bib_file = "bibliography
   message("Bibliography has been successfully written to ", bib_file)
 }
 
-#' Title
+#' Import References to your Excel-File
 #'
 #' @param excel_file_path The name or directory of the Excel file you use for your References. In the file, the bibtex-entries should be arranged in a column, one entry per cell.
 #' @param bib_file_path The name of your .bib-file. Defaults to "export.bib".
 #' @param sheet_name Sheet name where you want the references imported to, default is "Import".
 #'
-#' @return References in your Excel file!
+#' @return References in your Excel file
 #' @export
+#' @import xlsx openxlsx utils
 #'
 #' @examples
 #' download.file(url="https://raw.githubusercontent.com/Enno-W/excelbib/main/Excel_References_example.xlsx", destfile = "Excel_References_example.xlsx", mode ="wb")
@@ -50,15 +44,7 @@ xlsx_to_bib <- function(excel_file = "References.xlsx", bib_file = "bibliography
 #' require("openxlsx")
 #' bib_to_xlsx(bib_file_path = "export_example.bib", excel_file_path = "Excel_References_example.xlsx")
 bib_to_xlsx <- function(bib_file_path = "export.bib", excel_file_path = "References.xlsx", sheet_name = "Import") {
-  if (!requireNamespace("openxlsx", quietly = TRUE)) {
-    stop(
-      "Package \"openxlsx\" must be installed to use this function.",
-      call. = FALSE
-    )
-  }
-  if (!"openxlsx" %in% loadedNamespaces()) {
-    library(openxlsx)
-  }
+
   bib_file <- readLines(bib_file_path, warn = FALSE)
   text <- paste(bib_file, collapse = " ")
   entries <- unlist(strsplit(text, split = "\\@"))
@@ -69,4 +55,27 @@ bib_to_xlsx <- function(bib_file_path = "export.bib", excel_file_path = "Referen
   openxlsx::writeData(wb, sheet_name, data, colNames = FALSE)
   openxlsx::saveWorkbook(wb, excel_file_path, overwrite = TRUE)
   message("Bibliography has been successfully written to ", excel_file_path)
+}
+
+#' Add an Empty Excel-File and Start using Excel as Reference Software
+#'
+#' @param xlsx_name The name you want to use for your Excel-file where you organize your references.
+#'
+#' @return
+#' @export
+#' @import utils
+#'
+#' @examples
+#' add_xlsx(xlsx_name= "Excel_References_example")
+add_xlsx <- function(xlsx_name = "References.xlsx"){
+
+  if (!file.exists(xlsx_name)) {
+    download.file(url = "https://raw.githubusercontent.com/Enno-W/excelbib/main/Excel_References_example.xlsx",
+                  destfile = xlsx_name,
+                  mode = "wb")
+    message("An empty Excel-file for your references was added")
+  } else {
+    message ("File already exists and will not be overwritten.")
+  }
+
 }
